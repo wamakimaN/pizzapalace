0da69403b4f94b326969e712ff9abcff71a37ffd
wamakimaN <wamamoses@gmail.com>
2019-11-11 11:08:46 +0300

    get user input for delivery address

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,20 +23,20 @@ Order.prototype.pizzaDetails = function () {
 //calculate the cost of a pizza
 //always return rounded up to nearest 10
 Order.prototype.pizzaPrice = function () {
-  return Math.ceil(((this.cheese+=this.veggies+=this.veggiesTwo+=this.meat+= this.sauce+= this.crust)*this.size)/10)*10;
+  return Math.ceil(((this.cheese += this.veggies += this.veggiesTwo += this.meat += this.sauce += this.crust) * this.size) / 10) * 10;
 }
 //add contents of our array together
 Order.prototype.finalCost = function () {
   let cartTotalPrice = 0;
-  for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement ++) {
+  for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement++) {
     cartTotalPrice += totalPriceArray[arrayElement];
   }
   return cartTotalPrice;
 }
 
 //user interface logic
-$(document).ready(function(){
-  $("form#order").submit(function(event){
+$(document).ready(function () {
+  $("form#order").submit(function (event) {
     event.preventDefault()
     //gather input from user
     let pizzaSize = $("#size option:selected").text();
@@ -46,9 +46,9 @@ $(document).ready(function(){
     let pveggiesTwo = $("#more-veggies option:selected").text();
     let pizzaCheese = $("#cheese option:selected").text();
     let pizzaMeat = $("#meat option:selected").text();
-    
+
     let newOrder = new Order(pizzaSize, pizzaCrust, pizzaCheese, pizzaSauce, pizzaVeggies, pveggiesTwo, pizzaMeat);
-    
+
     //get value of selected ingredients
     let costSize = parseFloat($("#size option:selected").val());
     let costCrust = parseInt($("#crust option:selected").val());
@@ -62,7 +62,7 @@ $(document).ready(function(){
     if ($("#more-veggies option:selected").val() === ("")) {
       costVeggies = 0;
     }
-    
+
     let pizzaCost = new Order(costSize, costCrust, costCheese, costSauce, costVeggy, costVeggies, costMeat);
 
     //ensure value is never re-declared!!
@@ -71,7 +71,21 @@ $(document).ready(function(){
     totalPriceArray.push(finalPrice);
     $("#order-details").show();
     $("#holder").hide()
-    $("#pizza-details").append("<ul><li>" + pizzaSize + finalPrice + "</li></ul>");
+    $("#pizza-details").append("<ul><li>" + pizzaSize + " ksh " + finalPrice + "</li></ul>");
     $("#final-cost").text(pizzaCost.finalCost());
+  })
+  $("#delivery").click(function (event) {
+    event.preventDefault();
+    $("#address").show();
+  })
+  $("form#address").submit(function (event) {
+    event.preventDefault();
+
+    let firstName = $("#firstName").val();
+    let lastName = $("#lastName").val();
+    let city = $("#cityName").val();
+    let streetAddress = $("#streetAddress").val();
+
+    $("#address").hide();
   })
 })
